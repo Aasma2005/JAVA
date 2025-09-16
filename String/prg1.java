b828fe11be463724b4108f9d0a1288a26ea0ff6d
Aasma2005 <aasmashaikh48237@gmail.com>
2025-09-16 09:21:10 -0700

    Add files via upload

---
+++ String/prg1.java
@@ -0,0 +1,11 @@
+package String;
+
+class StringDemo{
+    public static void main(String[] args) {
+        String str="Aasma";
+        System.out.println(str);
+
+        char arr[]=new char[]{'a','b','c','d','e','f'};
+        System.out.println(arr);
+    }
+}
---
+++ String/prg2.java
@@ -0,0 +1,23 @@
+
+package String;
+
+public class prg2 {
+    public static void main(String[] args) {
+        int[] arr1=new int[]{10,20,30,40,50};
+        System.out.println(arr1);
+
+        char arr2[]=new char[]{'A','B','C','D','E'};
+        System.out.println(arr2);
+        System.out.println(arr2[2]);
+
+
+        float[] arr3=new float[]{10,20,30,40,50};
+        System.out.println(arr3);
+
+        byte[] arr4=new byte[]{11,22,34,43,52};
+        System.out.println(arr4);
+
+       
+    }
+    
+}
