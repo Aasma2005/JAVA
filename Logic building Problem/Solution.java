63b37a44090f36ca3c31770141723bf3c9d9722f
Aasma2005 <aasmashaikh48237@gmail.com>
2026-03-01 21:52:03 +0530

    Add files via upload

---
+++ Logic building Problem/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void printDiamond(int n) {
+
+        // Upper half
+        for(int i = 1; i <= n; i++) {
+
+            // spaces
+            for(int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int j = 1; j <= (2*i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for(int i = n-1; i >= 1; i--) {
+
+            // spaces
+            for(int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int j = 1; j <= (2*i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        int n = 3;
+        Solution sc = new Solution();
+        sc.printDiamond(n);
+    }
+}
