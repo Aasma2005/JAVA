a50db40cb0d78eb182f07ae9d82fd0d15b088e22
Aasma2005 <aasmashaikh48237@gmail.com>
2026-03-04 22:59:57 +0530

    Add files via upload

---
+++ Logic building Problem/Demo.java
@@ -0,0 +1,11 @@
+class Demo {
+    static void printHelloWorld() {
+        
+        System.out.println("Hello World");
+        
+    }
+    public static void main(String[]args){
+        Demo sc=new Demo();
+        sc.printHelloWorld();
+    }
+}
