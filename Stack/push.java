638282f3f2a64ed62f3a4ae1cc220d05120da51b
Aasma2005 <aasmashaikh48237@gmail.com>
2026-03-05 12:01:16 +0530

    Add files via upload

---
+++ Stack/push.java
@@ -0,0 +1,21 @@
+package Stack;
+
+import java.util.*;
+class push{
+    public static void main(String[] args)
+    {
+        // Create a new stack
+        Stack<Integer> s = new Stack<>();
+
+        // Push elements onto the stack
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        // Pop elements from the stack
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+}
---
+++ Stack/removing.java
@@ -0,0 +1,34 @@
+package Stack;
+
+import java.util.Stack;
+
+public class removing {
+     public static void main(String args[])
+    {
+        // Creating an empty Stack
+        Stack<Integer> stack = new Stack<Integer>();
+
+        stack.push(10);
+        stack.push(15);
+        stack.push(30);
+        stack.push(20);
+        stack.push(5);
+
+        System.out.println("Initial Stack: " + stack);
+
+        System.out.println("Popped element: "+ stack.pop());
+        System.out.println("Popped element: "+ stack.pop());
+
+        System.out.println("Stack after pop operation "+ stack);
+
+        System.out.println("Is stack empty? "+ stack.empty());
+        // Pop remaining elements
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        // Check if the stack is empty
+        System.out.println("Is stack empty? "
+                           + stack.empty());
+    }
+}
