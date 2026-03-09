b88aad3e7e28ae9b384907c3e43561647fd92a34
Aasma2005 <aasmashaikh48237@gmail.com>
2026-03-09 18:54:24 +0530

    Add files via upload

---
+++ Logic building Problem/Prime.java
@@ -0,0 +1,11 @@
+class Prime{
+    public static void main(String[]args){
+        int n=7;
+        for(int i=2;i<=n/2;i++){
+        if(n% i==0){
+            System.out.println("false");
+        }
+    }
+    System.out.println("true");
+    }
+}
